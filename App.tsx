6006c594ce2ea0d65648c5b62d29d78048663112
anjaligurjar <anjaligurjar861@gmail.com>
2023-05-16 19:01:21 +0530

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import Dashboard from './dashboard';
 import './style.css';
 
 export default function App() {
   return (
     <div>
-<
+<Dashboard />
+
       
       </div>
   );
--- dashboard.tsx
+++ dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import react, { useState } from 'react';
 
 const Dashboard = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -37,7 +37,7 @@ const Dashboard = () => {
       {showRegister ? (
         <div>
           <h3>Register Panel</h3>
-          {/* Add your registration component or form here */}
+        
         </div>
       ) : (
         <button onClick={handleToggleRegister}>Show Register</button>
